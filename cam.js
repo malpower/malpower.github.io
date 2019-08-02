2ff63a097fd8d8aa0b5126d089b72ec96e5bee87
Thrasky Louh <malpower@ymail.com>
2019-08-02 17:58:39 +0800

    sdfdsfds

--- cam.js
+++ cam.js
@@ -6,32 +6,32 @@ function log(txt)
 window.onload=async ()=>
 {
     log("JJJ");
-    navigator.getUserMedia = navigator.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia;
+    // navigator.getUserMedia = navigator.getUserMedia ||
+    //     navigator.webkitGetUserMedia ||
+    //     navigator.mozGetUserMedia;
 
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
-            function (stream) {
+    // if (navigator.getUserMedia) {
+    //     navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
+    //         function (stream) {
                 
-                var video = document.getElementById("ok");
-                try
-                {
-                    video.src = window.URL.createObjectURL(stream);
-                }
-                catch (e)
-                {
-                    video.srcObject=stream;
-                }
-                video.onloadedmetadata = function (e) {
-                    video.play();
-                };
-            },
-            function (err) {
-                log("The following error occurred: " + err.name);
-            }
-        );
-    } else {
-        log("getUserMedia not supported");
-    }
+    //             var video = document.getElementById("ok");
+    //             try
+    //             {
+    //                 video.src = window.URL.createObjectURL(stream);
+    //             }
+    //             catch (e)
+    //             {
+    //                 video.srcObject=stream;
+    //             }
+    //             video.onloadedmetadata = function (e) {
+    //                 video.play();
+    //             };
+    //         },
+    //         function (err) {
+    //             log("The following error occurred: " + err.name);
+    //         }
+    //     );
+    // } else {
+    //     log("getUserMedia not supported");
+    // }
 }
